d147866e722b790cf4adef7b288f0cc5eab6ca16
King <98532667+Kingofpig151251@users.noreply.github.com>
2024-05-08 13:49:27 +0800

    backup

--- server/server.js
+++ server/server.js
@@ -1,37 +1,42 @@
 // server.js
-'use strict';
+"use strict";
 
 //#region Imports
-const WebSocket = require('ws');
-const http = require('http');
-const express = require('express');
+const WebSocket = require("ws");
+const http = require("http");
+const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const MESSAGE_TYPES = require('../public/MESSAGE_TYPES');
-const Element_ID = require('../public/Element_ID');
-const Troop = require('./Troop');
-const { type } = require('os');
-app.use(express.static('public'));
-const opn = require('opn');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Proxy_War');
+const MESSAGE_TYPES = require("../public/MESSAGE_TYPES");
+const Element_ID = require("../public/Element_ID");
+const Troop = require("./Troop");
+const { type } = require("os");
+app.use(express.static("public"));
+const opn = require("opn");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Proxy_War", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 //#endregion
 
 const chatSchema = new mongoose.Schema({
   name: String,
   message: String,
-  timestamp: Date
+  timestamp: Date,
 });
 
-const Chat = mongoose.model('Chat', chatSchema);
+const Chat = mongoose.model("Chat", chatSchema);
 
 const STATE = {
-  MATCHING: 'Matching',
-  ATTACKER_TURN: 'Attacker Turn',
-  DEFENDER_TURN: 'Defender Turn',
-  BATTLING: 'Battling',
-}
+  MATCHING: "Matching",
+  ATTACKER_TURN: "Attacker Turn",
+  DEFENDER_TURN: "Defender Turn",
+  BATTLING: "Battling",
+};
 
 let wattingList = [];
 let attacker, defender, attackerTroop, defenderTroop;
@@ -41,12 +46,11 @@ let round = 0;
 const DELAY_INCREMENT = 1000; // 1 second
 const ATTRIBUTE_MULTIPLIER = 0.5;
 
-wss.on('connection', async (ws) => {
-
-  ws.on('message', async (message) => {
+wss.on("connection", async (ws) => {
+  ws.on("message", async (message) => {
     var jsonObj = JSON.parse(message);
-    console.log('received', jsonObj);
-    if (jsonObj.message === '') {
+    console.log("received", jsonObj);
+    if (jsonObj.message === "") {
       return;
     }
     switch (jsonObj.type) {
@@ -55,74 +59,100 @@ wss.on('connection', async (ws) => {
           attacker = ws;
         } else if (!defender && ws !== attacker) {
           defender = ws;
-        } else if (!wattingList.includes(ws) && ws !== attacker && ws !== defender) {
+        } else if (
+          !wattingList.includes(ws) &&
+          ws !== attacker &&
+          ws !== defender
+        ) {
           wattingList.push(ws);
         } else {
-          ws.send(JSON.stringify({ type: MESSAGE_TYPES.SYSTEM_MESSAGE, message: 'You are already in the game.' }));
+          ws.send(
+            JSON.stringify({
+              type: MESSAGE_TYPES.SYSTEM_MESSAGE,
+              message: "You are already in the game.",
+            })
+          );
           return;
         }
-        broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${ws.name} has joined the game.`);
+        broadcastMessage(
+          MESSAGE_TYPES.SYSTEM_MESSAGE,
+          `${ws.name} has joined the game.`
+        );
         break;
       case MESSAGE_TYPES.SET_NAME:
         //Set the name of the player
         ws.name = jsonObj.message;
         // disable the name input
-        ws.send(JSON.stringify({
-          type: MESSAGE_TYPES.SET_NAME,
-          message: ws.name
-        }));
+        ws.send(
+          JSON.stringify({
+            type: MESSAGE_TYPES.SET_NAME,
+            message: ws.name,
+          })
+        );
         //Send a message to all clients that a new player has joined
         wss.clients.forEach((client) => {
-          client.send(JSON.stringify({
-            type: MESSAGE_TYPES.SYSTEM_MESSAGE,
-            message: `${ws.name} has joined the chat.`
-          }));
+          client.send(
+            JSON.stringify({
+              type: MESSAGE_TYPES.SYSTEM_MESSAGE,
+              message: `${ws.name} has joined the chat.`,
+            })
+          );
         });
         break;
       case MESSAGE_TYPES.CHAT_MESSAGE:
-        //Create a new conversation record
-        const chat = new Chat({
-          name: ws.name,
-          message: jsonObj.message,
-          timestamp: new Date()
-        });
-        //Save to database
-        await chat.save();
+        saveChatAndSendMessage(ws, jsonObj);
         //Send the message to all clients
-        wss.clients.forEach((client) => {
-          client.send(JSON.stringify({
-            type: MESSAGE_TYPES.CHAT_MESSAGE,
-            message: jsonObj.message,
-            name: ws.name,
-            timestamp: chat.timestamp
-          }));
-        });
+   
         break;
       case MESSAGE_TYPES.BATTLE:
         if (ws === attacker && state === STATE.ATTACKER_TURN) {
           try {
             attackerTroop.updateFromBattleMessage(jsonObj);
-            broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${attackerTroop.name} has finished the turn, the amount invested is ${attackerTroop.force + attackerTroop.arms + attackerTroop.food}. With skill ${attackerTroop.skill}`);
+            broadcastMessage(
+              MESSAGE_TYPES.SYSTEM_MESSAGE,
+              `${
+                attackerTroop.name
+              } has finished the turn, the amount invested is ${
+                attackerTroop.force + attackerTroop.arms + attackerTroop.food
+              }. With skill ${attackerTroop.skill}`
+            );
             state = STATE.DEFENDER_TURN;
             broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `Now is ${state}.`);
           } catch (error) {
-            ws.send(JSON.stringify({ type: MESSAGE_TYPES.SYSTEM_MESSAGE, message: error.message }));
+            ws.send(
+              JSON.stringify({
+                type: MESSAGE_TYPES.SYSTEM_MESSAGE,
+                message: error.message,
+              })
+            );
           }
         } else if (ws === defender && state === STATE.DEFENDER_TURN) {
           try {
             defenderTroop.updateFromBattleMessage(jsonObj);
-            broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${defenderTroop.name} has finished the turn, the amount invested is ${defenderTroop.force + defenderTroop.arms + defenderTroop.food}. With skill ${defenderTroop.skill}`);
+            broadcastMessage(
+              MESSAGE_TYPES.SYSTEM_MESSAGE,
+              `${
+                defenderTroop.name
+              } has finished the turn, the amount invested is ${
+                defenderTroop.force + defenderTroop.arms + defenderTroop.food
+              }. With skill ${defenderTroop.skill}`
+            );
             state = STATE.BATTLING;
             updateLabel();
             await compare(attackerTroop, defenderTroop);
             broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `Now is ${state}.`);
           } catch (error) {
-            ws.send(JSON.stringify({ type: MESSAGE_TYPES.SYSTEM_MESSAGE, message: error.message }));
+            ws.send(
+              JSON.stringify({
+                type: MESSAGE_TYPES.SYSTEM_MESSAGE,
+                message: error.message,
+              })
+            );
           }
         }
         break;
       default:
-        console.log('Unknown message type', type);
+        console.log("Unknown message type", type);
     }
     addPlayerToBattle();
     if (attacker && defender && state === STATE.MATCHING) {
@@ -135,7 +165,7 @@ wss.on('connection', async (ws) => {
     updataWaitingList();
   });
 
-  ws.on('close', async () => {
+  ws.on("close", async () => {
     // broadcast who are disconnected
     if (wss.clients.size === 0) {
       attacker = null;
@@ -145,15 +175,24 @@ wss.on('connection', async (ws) => {
       round = 0;
       state = STATE.MATCHING;
     }
-    broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${ws.name} has disconnected.`);
+    broadcastMessage(
+      MESSAGE_TYPES.SYSTEM_MESSAGE,
+      `${ws.name} has disconnected.`
+    );
     if (state != STATE.MATCHING) {
       let name = ws === attacker ? defender.name : attacker.name;
       if (attacker === ws) {
-        broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${attacker.name} has escaped. ${name} win the game.`);
+        broadcastMessage(
+          MESSAGE_TYPES.SYSTEM_MESSAGE,
+          `${attacker.name} has escaped. ${name} win the game.`
+        );
         attacker = null;
       }
       if (defender === ws) {
-        broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${defender.name} has escaped. ${name} win the game.`);
+        broadcastMessage(
+          MESSAGE_TYPES.SYSTEM_MESSAGE,
+          `${defender.name} has escaped. ${name} win the game.`
+        );
         defender = null;
       }
       addPlayerToBattle();
@@ -170,18 +209,19 @@ wss.on('connection', async (ws) => {
     updataWaitingList();
   });
 
-
   // Get all chat records from the database
   const chatRecords = await Chat.find({});
 
   // Send each chat record to the new user
-  chatRecords.forEach(record => {
-    ws.send(JSON.stringify({
-      type: MESSAGE_TYPES.CHAT_MESSAGE,
-      name: record.name,
-      message: record.message,
-      timestamp: record.timestamp
-    }));
+  chatRecords.forEach((record) => {
+    ws.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.CHAT_MESSAGE,
+        name: record.name,
+        message: record.message,
+        timestamp: record.timestamp,
+      })
+    );
   });
 
   updateLabel();
@@ -191,18 +231,22 @@ wss.on('connection', async (ws) => {
 // Function to broadcast a message to all connected clients
 function broadcastMessage(type, message) {
   wss.clients.forEach((client) => {
-    client.send(JSON.stringify({
-      type: type,
-      message: message
-    }));
+    client.send(
+      JSON.stringify({
+        type: type,
+        message: message,
+      })
+    );
   });
 }
 
 // Function to return the funding of a client's troop
 function returnFunding(client) {
   if (attackerTroop !== undefined && defenderTroop !== undefined) {
-    if (client === attacker && attackerTroop !== null) return attackerTroop.funding;
-    if (client === defender && defenderTroop !== null) return defenderTroop.funding;
+    if (client === attacker && attackerTroop !== null)
+      return attackerTroop.funding;
+    if (client === defender && defenderTroop !== null)
+      return defenderTroop.funding;
     return 0;
   }
   return 0;
@@ -210,9 +254,9 @@ function returnFunding(client) {
 
 // Function to update the label for all connected clients
 function updateLabel() {
-  wss.clients.forEach(client => {
-    const hostName = attacker ? attacker.name : 'Waiting';
-    const guestName = defender ? defender.name : 'Waiting';
+  wss.clients.forEach((client) => {
+    const hostName = attacker ? attacker.name : "Waiting";
+    const guestName = defender ? defender.name : "Waiting";
     const data = JSON.stringify({
       type: MESSAGE_TYPES.UPDATA_LABEL,
       value: {
@@ -232,7 +276,7 @@ function updateLabel() {
 // Function to update the waiting list for all connected clients
 function updataWaitingList() {
   clearWaitingList();
-  wss.clients.forEach(client => {
+  wss.clients.forEach((client) => {
     for (let name of wattingList) {
       const data = JSON.stringify({
         type: MESSAGE_TYPES.UPDATA_WAITING_LIST,
@@ -245,7 +289,7 @@ function updataWaitingList() {
 
 // Function to clear the waiting list for all connected clients
 function clearWaitingList() {
-  wss.clients.forEach(client => {
+  wss.clients.forEach((client) => {
     const data = JSON.stringify({
       type: MESSAGE_TYPES.CLEAR_WAITTING_LIST,
     });
@@ -257,72 +301,98 @@ function startGame() {
   attackerTroop = new Troop(attacker.name, 100);
   defenderTroop = new Troop(defender.name, 90);
   //send ready to battle message all clients
-  broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${attacker.name}(attacker) and ${defender.name}(defender) are in battle.`);
+  broadcastMessage(
+    MESSAGE_TYPES.SYSTEM_MESSAGE,
+    `${attacker.name}(attacker) and ${defender.name}(defender) are in battle.`
+  );
   state = STATE.ATTACKER_TURN;
   round = 1;
 }
 
 // Function to set the disabled state for all connected clients
 async function setDisabled() {
-  wss.clients.forEach(client => {
-    const data = JSON.stringify({ type: MESSAGE_TYPES.SET_DISABLED, value: true });
+  wss.clients.forEach((client) => {
+    const data = JSON.stringify({
+      type: MESSAGE_TYPES.SET_DISABLED,
+      value: true,
+    });
     client.send(data);
   });
   switch (state) {
     case STATE.ATTACKER_TURN:
-      attacker.send(JSON.stringify({ type: MESSAGE_TYPES.SET_DISABLED, value: false }));
+      attacker.send(
+        JSON.stringify({ type: MESSAGE_TYPES.SET_DISABLED, value: false })
+      );
       break;
     case STATE.DEFENDER_TURN:
-      defender.send(JSON.stringify({ type: MESSAGE_TYPES.SET_DISABLED, value: false }));
+      defender.send(
+        JSON.stringify({ type: MESSAGE_TYPES.SET_DISABLED, value: false })
+      );
       break;
   }
 }
 
-
 // Function to compare the attributes of attacker's troop and defender's troop
 async function compare(attackerTroop, defenderTroop) {
-  let attackerTotal = attackerTroop.force + attackerTroop.arms + attackerTroop.food;
-  let defenderTotal = defenderTroop.force + defenderTroop.arms + defenderTroop.food;
+  let attackerTotal =
+    attackerTroop.force + attackerTroop.arms + attackerTroop.food;
+  let defenderTotal =
+    defenderTroop.force + defenderTroop.arms + defenderTroop.food;
   attackerTroop.winAttributes = 0;
   defenderTroop.winAttributes = 0;
-  await compareAttribute(attackerTroop, defenderTroop, 'force');
-  await compareAttribute(attackerTroop, defenderTroop, 'arms');
-  await compareAttribute(attackerTroop, defenderTroop, 'food');
+  await compareAttribute(attackerTroop, defenderTroop, "force");
+  await compareAttribute(attackerTroop, defenderTroop, "arms");
+  await compareAttribute(attackerTroop, defenderTroop, "food");
 
   //check host skill
   switch (attackerTroop.skill) {
-    case 'NB':
+    case "NB":
       attackerTotal += attackerTroop.funding;
       break;
-    case 'QE':
-      attackerTroop.winAttributes = attackerTroop.winAttributes <= 3 ? attackerTroop.winAttributes + 1 : attackerTroop.winAttributes;
+    case "QE":
+      attackerTroop.winAttributes =
+        attackerTroop.winAttributes <= 3
+          ? attackerTroop.winAttributes + 1
+          : attackerTroop.winAttributes;
       break;
   }
 
   switch (defenderTroop.skill) {
-    case 'NB':
+    case "NB":
       defenderTotal += defenderTroop.funding;
       break;
-    case 'QE':
-      defenderTroop.winAttributes = defenderTroop.winAttributes <= 3 ? defenderTroop.winAttributes + 1 : defenderTroop.winAttributes;
+    case "QE":
+      defenderTroop.winAttributes =
+        defenderTroop.winAttributes <= 3
+          ? defenderTroop.winAttributes + 1
+          : defenderTroop.winAttributes;
       break;
   }
 
-  if (attackerTroop.skill === 'IC') {
+  if (attackerTroop.skill === "IC") {
     defenderTotal *= defenderTroop.winAttributes * 0.5;
   } else {
     defenderTotal *= defenderTroop.winAttributes;
   }
 
-  if (defenderTroop.skill === 'IC') {
+  if (defenderTroop.skill === "IC") {
     attackerTotal *= attackerTroop.winAttributes * 0.5;
   } else {
     attackerTotal *= attackerTroop.winAttributes;
   }
 
-  await delayBroadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `Settlement incoming...`);
-  await delayBroadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${attackerTroop.name} get ${attackerTotal} income.`);
-  await delayBroadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${defenderTroop.name} get ${defenderTotal} income.`);
+  await delayBroadcastMessage(
+    MESSAGE_TYPES.SYSTEM_MESSAGE,
+    `Settlement incoming...`
+  );
+  await delayBroadcastMessage(
+    MESSAGE_TYPES.SYSTEM_MESSAGE,
+    `${attackerTroop.name} get ${attackerTotal} income.`
+  );
+  await delayBroadcastMessage(
+    MESSAGE_TYPES.SYSTEM_MESSAGE,
+    `${defenderTroop.name} get ${defenderTotal} income.`
+  );
 
   if (attackerTroop.winAttributes != 0) {
     attackerTroop.funding += attackerTotal;
@@ -343,9 +413,15 @@ async function compare(attackerTroop, defenderTroop) {
 async function checkWhoWin() {
   if (round > 3) {
     if (attackerTroop.funding > defenderTroop.funding) {
-      broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${attackerTroop.name} win the game.`);
+      broadcastMessage(
+        MESSAGE_TYPES.SYSTEM_MESSAGE,
+        `${attackerTroop.name} win the game.`
+      );
     } else if (attackerTroop.funding < defenderTroop.funding) {
-      broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `${defenderTroop.name} win the game.`);
+      broadcastMessage(
+        MESSAGE_TYPES.SYSTEM_MESSAGE,
+        `${defenderTroop.name} win the game.`
+      );
     } else {
       broadcastMessage(MESSAGE_TYPES.SYSTEM_MESSAGE, `No one win the game.`);
     }
@@ -360,8 +436,7 @@ async function checkWhoWin() {
 
 // if host is null, set the waitting list player to host, if guest is null, set the second player to guest
 function addPlayerToBattle() {
-  if (wattingList.length === 0)
-    return;
+  if (wattingList.length === 0) return;
   if (attacker === null) {
     attacker = wattingList.shift();
   }
@@ -371,7 +446,7 @@ function addPlayerToBattle() {
 }
 
 async function delayBroadcastMessage(type, message) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       broadcastMessage(type, message);
       resolve();
@@ -384,9 +459,11 @@ async function compareAttribute(attackerTroop, defenderTroop, attribute) {
   let messages = [
     `${attribute} battling... `,
     `${attacker.name} :${attackerTroop[attribute]} vs ${defender.name} :${defenderTroop[attribute]}`,
-    attackerTroop[attribute] > defenderTroop[attribute] ? `${attacker.name} win ${attribute} battle.` :
-      attackerTroop[attribute] < defenderTroop[attribute] ? `${defender.name} win ${attribute} battle.` :
-        `No one win ${attribute} battle.`
+    attackerTroop[attribute] > defenderTroop[attribute]
+      ? `${attacker.name} win ${attribute} battle.`
+      : attackerTroop[attribute] < defenderTroop[attribute]
+      ? `${defender.name} win ${attribute} battle.`
+      : `No one win ${attribute} battle.`,
   ];
 
   for (let message of messages) {
@@ -400,8 +477,29 @@ async function compareAttribute(attackerTroop, defenderTroop, attribute) {
   }
 }
 
+async function saveChatAndSendMessage(ws, jsonObj) {
+  const chat = new Chat({
+    name: ws.name,
+    message: jsonObj.message,
+    timestamp: new Date(),
+  });
+
+  await chat.save();
+
+  wss.clients.forEach((client) => {
+    client.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.CHAT_MESSAGE,
+        message: jsonObj.message,
+        name: ws.name,
+        timestamp: new Date(),
+      })
+    );
+  });
+}
+
 server.listen(8080, () => {
-  console.log('Listening on http://localhost:8080');
-  opn('http://localhost:8080');
-  opn('http://localhost:8080');
+  console.log("Listening on http://localhost:8080");
+  opn("http://localhost:8080");
+  opn("http://localhost:8080");
 });
